ce0dbf7a1dedfe0dcc411126019c07002f09ac1d
Brandon Thetford <github@snapsinazfs.com>
2025-11-04 22:29:20 -0700

    Add MediaAsset type that was added to SnapPackage

---
+++ PSnaps/SnapCore/MediaAsset.cs
@@ -0,0 +1,25 @@
+#region COPYRIGHT
+// Copyright 2025 Brandon Thetford
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+// 
+// A copy of the license is also available in the repository on GitHub at https://github.com/dodexahedron/PSnaps/blob/master/LICENSE.
+#endregion
+
+namespace PSnaps.SnapCore;
+
+[PublicAPI]
+public class MediaAsset
+{
+  [JsonPropertyName ( "height" )]
+  public int? Height { get; set; }
+
+  [JsonPropertyName ( "type" )]
+  public string Type { get; set; }
+
+  [JsonPropertyName ( "url" )]
+  public string Url { get; set; }
+
+  [JsonPropertyName ( "width" )]
+  public int? Width { get; set; }
+}
