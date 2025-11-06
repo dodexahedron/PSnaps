f5222efb137eb9096d9db69258288e2450dcb438
Brandon Thetford <github@snapsinazfs.com>
2025-11-06 15:41:36 -0700

    Add enum for media asset kind

---
+++ PSnaps/SnapCore/MesiaAssetKind.cs
@@ -0,0 +1,21 @@
+#region COPYRIGHT
+// Copyright 2025 Brandon Thetford
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+// 
+// A copy of the license is also available in the repository on GitHub at https://github.com/dodexahedron/PSnaps/blob/master/LICENSE.
+#endregion
+
+namespace PSnaps.SnapCore;
+
+public enum MediaAssetKind
+{
+  [JsonStringEnumMemberName ( "icon" )]
+  Icon,
+
+  [JsonStringEnumMemberName ( "banner" )]
+  Banner,
+
+  [JsonStringEnumMemberName ( "screenshot" )]
+  Screenshot
+}
