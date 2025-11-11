a0683a582b50c5650ee30e731c23cf5b3a75c2ee
Brandon Thetford <github@snapsinazfs.com>
2025-11-11 03:05:13 -0700

    Add Get-SnapdClient cmdlet

---
+++ PSnaps/Cmdlets/GetSnapdClientCommand.cs
@@ -0,0 +1,27 @@
+#region COPYRIGHT
+// Copyright 2025 Brandon Thetford
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+// 
+// A copy of the license is also available in the repository on GitHub at https://github.com/dodexahedron/PSnaps/blob/master/LICENSE.
+#endregion
+
+using System.Management.Automation;
+using PSnaps.SnapdRestApi.Clients;
+
+namespace PSnaps.Cmdlets;
+
+[Cmdlet ( VerbsCommon.Get, Noun )]
+[OutputType ( typeof( ISnapdRestClient ) )]
+[PublicAPI]
+public sealed class GetSnapdClientCommand : SnapdClientCmdlet
+{
+  public const string Noun = "SnapdClient";
+
+  /// <inheritdoc />
+  [ExcludeFromCodeCoverage]
+  protected override void ProcessRecord ( )
+  {
+    WriteObject ( ApiClient );
+  }
+}
